2c3d4d44de0a1d6083b375e0be1412673a0b0906
Brandon <73306165+BrandonBautistaSantana0402@users.noreply.github.com>
2021-01-05 12:05:22 -0500

    Update cumple.php

--- app/Http/Controllers/cumple.php
+++ app/Http/Controllers/cumple.php
@@ -2,17 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
+use App\Helpers\Helpers;
 
 class cumple extends Controller
 {
     
     public function index()
         {
-            //return view('mision.index');
-            return view('cumple.index');
+            $user = User::all();
             
+            $calendario =Helpers::usuario($user);
+            return view('cumple.index',compact('calendario'));            
         }
-    
-    
+            
 }
